beecab68205263713bf54e81266e2cd913b8f703
Raffa Arya Nugraha <84084497+raffanugraha17@users.noreply.github.com>
2024-03-04 00:54:46 +0700

    upload file

--- triangle-pos-master-finale/resources/views/layouts/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="c-footer">
-    <div>GT Apps© {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://google.com">GT Apps</a></strong></div>
-
-    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
-</footer>
