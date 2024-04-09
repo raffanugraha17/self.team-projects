af7c8839f993fc49d957f241dd5780e65752e160
Raffa Arya Nugraha <84084497+raffanugraha17@users.noreply.github.com>
2024-04-09 16:24:12 +0700

    main

--- POSFIX/resources/js/app.js
+++
@@ -1,6 +0,0 @@
-window.closeModals = () => {
-    const openCreateModal = document.querySelector('.filament-modal.filament-modal-is-open[x-data*="createModal"]')
-    if (!openCreateModal) {
-        Alpine.store('main').closeModals()
-    }
-}
