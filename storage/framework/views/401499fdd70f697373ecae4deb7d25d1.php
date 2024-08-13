b0d25ad4a721aec3911c5d2617000915584cc594
Raffa Arya Nugraha <84084497+raffanugraha17@users.noreply.github.com>
2024-08-13 22:21:47 +0800

    update

---
+++ storage/framework/views/401499fdd70f697373ecae4deb7d25d1.php
@@ -0,0 +1,22 @@
+<?php if (isset($component)) { $__componentOriginalbe23554f7bded3778895289146189db7 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalbe23554f7bded3778895289146189db7 = $attributes; } ?>
+<?php $component = Filament\View\LegacyComponents\Page::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('filament::page'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Filament\View\LegacyComponents\Page::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+    <?php echo QrCode::size(200)->generate($record->name); ?>
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalbe23554f7bded3778895289146189db7)): ?>
+<?php $attributes = $__attributesOriginalbe23554f7bded3778895289146189db7; ?>
+<?php unset($__attributesOriginalbe23554f7bded3778895289146189db7); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalbe23554f7bded3778895289146189db7)): ?>
+<?php $component = $__componentOriginalbe23554f7bded3778895289146189db7; ?>
+<?php unset($__componentOriginalbe23554f7bded3778895289146189db7); ?>
+<?php endif; ?><?php /**PATH /Users/raffaryan/Desktop/POSFIX/resources/views/filament/resources/membership-resource/pages/view-qr-code.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8171edbcb58588d856575d751c545e9e.php
@@ -0,0 +1,8 @@
+<h4
+    <?php echo e($attributes->class(['fi-ta-empty-state-heading text-base font-semibold leading-6 text-gray-950 dark:text-white'])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</h4>
+<?php /**PATH /Users/raffaryan/Desktop/POSFIX/resources/views/vendor/filament-tables/components/empty-state/heading.blade.php ENDPATH**/ ?>
